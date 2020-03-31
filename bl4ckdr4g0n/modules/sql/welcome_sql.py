03332b90aa8a6a382ee071eb7f8b3abbf47222b6
ThakorRahul <56543994+ThakorRahul@users.noreply.github.com>
2020-03-31 23:21:49 +0530

    Update welcome_sql.py

--- bl4ckdr4g0n/modules/sql/welcome_sql.py
+++ bl4ckdr4g0n/modules/sql/welcome_sql.py
@@ -6,8 +6,9 @@ from sqlalchemy import Column, String, Boolean, UnicodeText, Integer, BigInteger
 from bl4ckdr4g0n.modules.helper_funcs.msg_types import Types
 from bl4ckdr4g0n.modules.sql import SESSION, BASE
 
-DEFAULT_WELCOME = "Hey {mention}, how are you?"
-DEFAULT_GOODBYE = "Nice to meet you!"
+DEFAULT_WELCOME = "Hey {first}, how are you?"
+DEFAULT_GOODBYE = "Nice knowing you!"
+
 
 class Welcome(BASE):
     __tablename__ = "welcome_pref"
